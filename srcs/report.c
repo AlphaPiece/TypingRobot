9185dab0f004bfb5ae7bbd0e473f9e606fe1aeec
Zexi Wang <twopieces0921@gmail.com>
2019-02-04 14:41:12 -0500

    version 2.6 updated

--- srcs/report.c
+++ srcs/report.c
@@ -6,7 +6,7 @@
 /*   By: Zexi Wang <twopieces0921@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/03 15:08:32 by Zexi Wang         #+#    #+#             */
-/*   Updated: 2019/02/04 11:16:46 by Zexi Wang        ###   ########.fr       */
+/*   Updated: 2019/02/04 14:38:34 by Zexi Wang        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,27 @@
 void	speed_level(int cps)
 {
 	if (cps >= 16)
-		ft_printf("Speed: " CYAN "%s\n" RESET, "S");
+		ft_printf("Speed: " CYAN "S\n" RESET);
 	else if (cps >= 8)
-		ft_printf("Speed: " MAGENTA "%s\n" RESET, "A");
+		ft_printf("Speed: " MAGENTA "A\n" RESET);
 	else if (cps >= 4)
-		ft_printf("Speed: " GREEN "%s\n" RESET, "B");
+		ft_printf("Speed: " GREEN "B\n" RESET);
 	else if (cps >= 2)
-		ft_printf("Speed: " YELLOW "%s\n" RESET, "C");
+		ft_printf("Speed: " YELLOW "C\n" RESET);
 	else
-		ft_printf("Speed: " RED "%s\n" RESET, "D");
+		ft_printf("Speed: " RED "D\n" RESET);
+}
+
+void	accuracy_level(int typo)
+{
+	if (typo == 0)
+		ft_printf("Accuracy: " MAGENTA "Perfect\n" RESET);
+	else if (typo == 1)
+		ft_printf("Accuracy: " GREEN "Good\n" RESET);
+	else if (typo == 2)
+		ft_printf("Accuracy: " YELLOW "Adequate\n", RESET);
+	else
+		ft_printf("Accuracy: " RED "Inadequate\n", RESET);
 }
 
 void	report(char *output, char *input, int len, int sec)
@@ -43,9 +55,9 @@ void	report(char *output, char *input, int len, int sec)
 			ft_printf(" ");
 	ft_printf(RESET "\n");
 	ft_putncharln('-', LIMIT);
-	ft_printf("Accuracy: %s\n" RESET, (typo > 0) ? RED "FAIL" : GREEN "GOOD");
 	ft_printf("Characters: %d\n", len);
 	ft_printf("Time: %ds\n", sec);
+	accuracy_level(typo);
 	if (sec == 0)
 		speed_level(len * 2 - len / 2);
 	else
